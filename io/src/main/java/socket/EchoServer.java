948459ecc9faaeae354a997abc35fdd2a0ccdf47
Klokov Stanislav <klokovstas2013@gmail.com>
2020-03-26 09:48:39 +0300

    0. Видео[#200499]. Fixed the content of branch

--- io/src/main/java/socket/EchoServer.java
+++ io/src/main/java/socket/EchoServer.java
@@ -5,28 +5,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final String byeMsg = "/?msg=bye";
+    private static final String BYE_MSG = "/?msg=bye";
     private static boolean closeServer = false;
 
     public static void main(String[] args) throws IOException {
 
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
+            while (!closeServer) {
                 Socket client = server.accept();
                 try (OutputStream out = client.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(client.getInputStream()))) {
                     String str;
-                    while (!(str = in.readLine()).isEmpty()) {
+                    str = in.readLine();
+                    while (!str.isEmpty()) {
                         System.out.println(str);
-                        if (str.toLowerCase().contains(byeMsg)) {
+                        if (str.toLowerCase().contains(BYE_MSG)) {
                             closeServer = true;
                         }
+                        str = in.readLine();
                     }
                     out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-                    if (closeServer) {
-                        break;
-                    }
                 }
             }
         }
