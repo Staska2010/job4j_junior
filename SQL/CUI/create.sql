70203999663952577c4468fcb02d086e58d2f8ac
Klokov Stanislav <klokovstas2013@gmail.com>
2020-04-13 12:56:33 +0300

    1. Схема прав пользователи и роли.[#200431] Исправлены ошибки в проекте базы

--- SQL/CUI/create.sql
+++ SQL/CUI/create.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE request_db;
-
+--create RULES table
 CREATE TABLE rules (
 id SERIAL PRIMARY KEY,
 read BOOLEAN,
@@ -7,70 +7,118 @@ write BOOLEAN,
 open BOOLEAN,
 execute BOOLEAN
 );
+-- insert data into RULES table
+INSERT INTO rules (read, write, open, execute) VALUES (true, true, true, true);
+INSERT INTO rules (read, write, open, execute) VALUES (true, false, false, false);
+INSERT INTO rules (read, write, open, execute) VALUES (true, false, true, false);
 
+--create ROLE table
 CREATE TABLE role (
+id SERIAL PRIMaRY KEY,
 role_name VARCHAR(50) PRIMARY KEY,
-rules INT REFERENCES rules(id)
 );
 
+--insert data into ROLE table
+INSERT INTO role role_name VALUES ('super_user');
+INSERT INTO role role_name VALUES ('read');
+INSERT INTO role role_name VALUES ('read_open');
+
+--create RoleRules table (many-to-many relationships)
+--composite primary key of Role and Rules
+CREATE TABLE RoleUsers (
+role INT,
+rules INT,
+PRIMARY KEY (role, rules);
+);
+
+--insert into RoleRules table (many-to-many relationships)
+INSERT INTO RoleRules VALUES (1, 1);
+INSERT INTO RoleRules VALUES (2, 2);
+INSERT INTO RoleRules VALUES (3, 3);
+
+
+--create USERS table
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 name VARCHAR(80),
 position VARCHAR(80),
-role VARCHAR(50) REFERENCES role(role_name)
+role VARCHAR(50) REFERENCES role(id)
 );
 
+--insert data into USERS table
+INSERT INTO users (name, position, role) VALUES ('John', 'CEO', 1);
+INSERT INTO users (name, position, role) VALUES ('Marie', 'storekeeper', 2);
+INSERT INTO users (name, position, role) VALUES ('Ruben', 'assistant', 3);
+
+
+--create CATEGORY table
 CREATE TABLE category (
 id  SERIAL PRIMARY KEY,
 cat_name VARCHAR(50)
 );
 
+--insert data into USERS table
+INSERT INTO category (cat_name) VALUES ('Check new project');
+
+
+--create COMMENT table
 CREATE TABLE comment (
 id SERIAL PRIMARY KEY,
 com_text TEXT
+item INT REFERENCES item(id)
 );
 
+--insert data into COMMENT table
+INSERT INTO comment (com_text, item) VALUES ('Simple comment, used in text field', 1);
+
+
+--create STATE table
 CREATE TABLE state (
-state VARCHAR(50) PRIMARY KEY
+id SERIAL PRIMARY KEY,
+state_name VARCHAR(50) PRIMARY KEY
 );
 
+--insert data into STATE table
+INSERT INTO state state_name VALUES ('Ready');
+
+
+--create ATTACHMENT table
 CREATE TABLE attachment (
-file VARCHAR(80) PRIMARY KEY
+id INT PRIMARY KEY,
+file VARCHAR(80),
+item INT REFERENCES item(id)
 );
 
+--insert data into ATTACHMENT table
+INSERT INTO attachment file, item VALUES ('path_to_attached_file', 1);
+
+
+--create ITEM table
 CREATE TABLE item (
-id PRIMARY KEY PRIMARY KEY,
-users int REFERENCES users(id),
+id INT PRIMARY KEY,
 item_name VARCHAR(50),
 date DATE,
-comment INT REFERENCES comment(id),
-attachment VARCHAR(80) REFERENCES attachment(file),
+users INT REFERENCES users(id),
 category INT REFERENCES category(id),
-state VARCHAR(50) REFERENCES state(state)
+state VARCHAR(50) REFERENCES state(id)
 );
 
-INSERT INTO rules (read, write, open, execute) VALUES (true, true, true, true);
-INSERT INTO rules (read, write, open, execute) VALUES (true, false, false, false);
-INSERT INTO rules (read, write, open, execute) VALUES (true, false, true, false);
 
-INSERT INTO role (role_name, rules) VALUES ('super_user', 1);
-INSERT INTO role (role_name, rules) VALUES ('read', 2);
-INSERT INTO role (role_name, rules) VALUES ('read_open', 3);
+--insert data into ITEM table
+INSERT INTO item (item_name, date, users, category, state)
+VALUES ('Check the project', '2020-01-01', 1, 1, 1);
 
-INSERT INTO users (name, position, role) VALUES ('John', 'CEO', 'super');
-INSERT INTO users (name, position, role) VALUES ('Marie', 'storekeeper', 'read_open');
-INSERT INTO users (name, position, role) VALUES ('Ruben', 'assistant', 'read');
 
-INSERT INTO comment (com_text) VALUES ('Simple comment, used in text field');
 
-INSERT INTO category (cat_name) VALUES ('Check new project');
 
-INSERT INTO state VALUES ('Ready');
 
-INSERT INTO attachment VALUES ('path_to_attached_file');
 
-INSERT INTO item (users, item_name, date, comment, attachment, category, state)
-VALUES (1, 'Check the project', '2020-01-01', 1, 'path_to_attached_file', 1,  'Ready');
+
+
+
+
+
+
 
 
 
