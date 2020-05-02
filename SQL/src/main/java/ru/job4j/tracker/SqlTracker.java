12b761eb437bd9d4c2e970c73a4f4ff6a2a9281b
Klokov Stanislav <klokovstas2013@gmail.com>
2020-05-02 23:51:40 +0300

    4.2. Трекер SQL[#200426]

--- SQL/src/main/java/ru/job4j/tracker/SqlTracker.java
+++ SQL/src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -1,14 +1,24 @@
 package ru.job4j.tracker;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class SqlTracker implements Store {
     private Connection cn;
+    private static final Logger LOG = LoggerFactory.getLogger(SqlTracker.class.getName());
 
+    public SqlTracker() {
+        init();
+    }
+
+    @Override
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
             Properties config = new Properties();
@@ -20,7 +30,7 @@ public class SqlTracker implements Store {
                     config.getProperty("password")
             );
         } catch (Exception e) {
-            throw new IllegalStateException(e);
+            LOG.error("Error in init block", e);
         }
     }
 
@@ -33,32 +43,101 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        return null;
+        String sqlString = "INSERT INTO item (name, descn) VALUES(?, ?);";
+        try (PreparedStatement pst = cn.prepareStatement(sqlString, Statement.RETURN_GENERATED_KEYS)) {
+            pst.setString(1, item.getName());
+            pst.setString(2, item.getDesc());
+            pst.executeUpdate();
+            ResultSet rs = pst.getGeneratedKeys();
+            int id = -1;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+            item.setId(Integer.toString(id));
+        } catch (SQLException e) {
+            LOG.error("Insert row error", e);
+        }
+        return item;
     }
 
     @Override
     public boolean replace(String id, Item item) {
-        return false;
+        int rowsQuantity = 0;
+        String sqlString = "UPDATE item SET name = ?, descn = ? WHERE id = ?;";
+        try (PreparedStatement pst = cn.prepareStatement(sqlString)) {
+            pst.setString(1, item.getName());
+            pst.setString(2, item.getDesc());
+            pst.setInt(3, Integer.parseInt(id));
+            rowsQuantity = pst.executeUpdate();
+        } catch (SQLException e) {
+            LOG.error("Update row error", e);
+        }
+        return (rowsQuantity > 0);
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        int rowsQuantity = 0;
+        String sqlString = "DELETE FROM item WHERE id = ?;";
+        try (PreparedStatement pst = cn.prepareStatement(sqlString)) {
+            pst.setInt(1, Integer.parseInt(id));
+            rowsQuantity = pst.executeUpdate();
+        } catch (SQLException e) {
+            LOG.error("Update row error", e);
+        }
+        return (rowsQuantity > 0);
     }
 
     @Override
     public List<Item> findAll() {
-        return null;
+        List<Item> result = new ArrayList<>();
+        String sqlString = "SELECT * FROM item;";
+        try (PreparedStatement pst = cn.prepareStatement(sqlString)) {
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Item newItem = new Item(rs.getString(2), rs.getString(3));
+                newItem.setId(String.valueOf(rs.getInt(1)));
+                result.add(newItem);
+            }
+        } catch (SQLException e) {
+            LOG.error("Error in find all block", e);
+        }
+        return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return null;
+        List<Item> result = new ArrayList<>();
+        String sqlString = "SELECT * FROM item WHERE name = ?;";
+        try (PreparedStatement pst = cn.prepareStatement(sqlString)) {
+            pst.setString(1, key);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Item newItem = new Item(rs.getString(2), rs.getString(3));
+                newItem.setId(String.valueOf(rs.getInt(1)));
+                result.add(newItem);
+            }
+        } catch (SQLException e) {
+            LOG.error("Error in find by name block", e);
+        }
+        return result;
     }
 
     @Override
     public Item findById(String id) {
-        return null;
+        Item result = null;
+        String sqlString = "SELECT * FROM item WHERE id = ?;";
+        try (PreparedStatement pst = cn.prepareStatement(sqlString)) {
+            pst.setInt(1, Integer.parseInt(id));
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                result = new Item(rs.getString(2), rs.getString(3));
+                result.setId(String.valueOf(rs.getInt(1)));
+            }
+        } catch (SQLException e) {
+            LOG.error("Error in find by ID block", e);
+        }
+        return result;
     }
 
 }
